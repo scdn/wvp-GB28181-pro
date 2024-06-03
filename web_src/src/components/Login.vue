7892a83cfac78c46956b69f818902d26ca1939d1
海小文 <43884938+scdn@users.noreply.github.com>
2024-06-03 14:17:27 +0800

    Update Login.vue

--- web_src/src/components/Login.vue
+++ web_src/src/components/Login.vue
@@ -3,7 +3,7 @@
   <div class="limiter">
     <div class="container-login100">
       <div class="wrap-login100">
-					<span class="login100-form-title p-b-26">WVP视频平台</span>
+					<span class="login100-form-title p-b-26">H5NVR智慧平台</span>
           <span class="login100-form-title p-b-48">
 						<i class="fa fa-video-camera"></i>
 					</span>
