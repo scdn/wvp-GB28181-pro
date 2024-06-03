22cfe886f748ae650e0c6b38f11363ed132a2d9f
海小文 <43884938+scdn@users.noreply.github.com>
2024-06-03 14:41:29 +0800

    Update 初始化-mysql.sql

--- 数据库/初始化-mysql.sql
+++ 数据库/初始化-mysql.sql
@@ -317,7 +317,7 @@ create table wvp_resources_tree (
 
 /*初始数据*/
 INSERT INTO wvp_user VALUES (1, 'admin','21232f297a57a5a743894a0e4a801fc3',1,'2021-04-13 14:14:57','2021-04-13 14:14:57','3e80d1762a324d5b0ff636e0bd16f1e3');
-INSERT INTO wvp_user_role VALUES (1, 'admin','0','2021-04-13 14:14:57','2021-04-13 14:14:57');
-
+INSERT INTO wvp_user_role VALUES (1, '管理员','0','2021-04-13 14:14:57','2021-04-13 14:14:57');
+INSERT INTO wvp_user_role VALUES (2, '操作员','0','2021-04-13 14:14:57','2021-04-13 14:14:57');
 
 
